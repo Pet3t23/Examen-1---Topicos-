d22d860e88f913cb1ef1231c760c54a7c23c3769
Alejandro Aldair Leon Resendiz <139192744+Pet3t23@users.noreply.github.com>
2024-02-15 17:54:38 -0600

    Add files via upload

---
+++ Matriz.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+/**
+ * @brief Función para calcular la matriz transpuesta de una matriz dada.
+ * @param matrix La matriz de entrada que se transpondrá.
+ */
+void transposeMatrix(int matrix[3][3]) {
+    int temp;
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = i + 1; j < 3; ++j) {
+            temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
+
+/**
+ * @brief Función para imprimir una matriz.
+ * @param matrix La matriz que se imprimirá.
+ */
+void printMatrix(int matrix[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int matrix[3][3];
+
+    std::cout << "Ingrese los elementos de la matriz 3x3:" << std::endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    std::cout << "Matriz original:" << std::endl;
+    printMatrix(matrix);
+
+    transposeMatrix(matrix);
+
+    std::cout << "Matriz transpuesta:" << std::endl;
+    printMatrix(matrix);
+
+    return 0;
+}
